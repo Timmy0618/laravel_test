7e428332b58cc2ff00bc80c54cf8f9072ffe3abb
Timmy <timmy85618@gmail.com>
2022-06-13 17:26:34 +0800

    init

---
+++ app/Console/Commands/test.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        dump(env('DB_HOST', 'test'));
+        return 0;
+    }
+}
